05f40f01febee6931c02d33c65835031b8e7d0c9
shmzzzz <slt0623@gmail.com>
2024-02-17 20:22:59 +0900

    Taskエンティティの追加

---
+++ app/src/main/java/com/example/todoapp/Task.kt
@@ -0,0 +1,12 @@
+package com.example.todoapp
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Task(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    @ColumnInfo var title: String,
+    @ColumnInfo var description: String,
+)
