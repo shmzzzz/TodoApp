dd23252110d8113422e439669498ceb4fbdbd064
shmz <85086833+shmzzzz@users.noreply.github.com>
2024-02-17 22:27:58 +0900

    feat: アプリデザインの大枠を作成する (#3)
    * Scaffoldの追加
    * タスク追加ダイアログの作成

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.todoapp.components.EditDialog
 import com.example.todoapp.ui.theme.TodoAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -23,8 +29,27 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    MainContent()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun MainContent() {
+    val isShowDialog = remember {
+        mutableStateOf(false)
+    }
+    if (isShowDialog.value) EditDialog(isShowDialog)
+
+    Scaffold(floatingActionButton = {
+        FloatingActionButton(
+            onClick = { isShowDialog.value = true },
+        ) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = "新規作成")
+        }
+    }) {
+
     }
 }
